8aacf28102afc9d3b8d7f5559011da8100eda868
DanielMonto <montoya.d@colegiomonterrosales.edu.co>
2024-05-31 19:46:41 -0500

    'field' field added to error responses

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -2,12 +2,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpRequest
 from rest_framework.permissions import AllowAny
-from .models import UserOwnModel,PasswordResetRequest
+from .models import UserOwnModel, PasswordResetRequest
 from rest_framework_simplejwt.tokens import AccessToken
 from django.template.loader import render_to_string
 from django.contrib.auth.hashers import make_password
 from apps.functions import are_keys_in_dict
-from .serializers import UserSerializer,MyTokenObtainPairSerializer
+from .serializers import UserSerializer, MyTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.core.mail import EmailMultiAlternatives
 from django.utils.html import strip_tags
@@ -18,65 +18,65 @@ class MyTokenObtainPairView(TokenObtainPairView):
         View for obtaining access and refresh tokens
     '''
     # Personalization of permissions for an unlogged client can login
-    permission_classes=[AllowAny]
-    serializer_class=MyTokenObtainPairSerializer
-    def post(self,request:HttpRequest):
+    permission_classes = [AllowAny]
+    serializer_class = MyTokenObtainPairSerializer
+    def post(self, request: HttpRequest):
         '''
             Post method for obtaining the tokens
         '''
         # Checks if all the keys are in dict
-        key_safes,error_message=are_keys_in_dict(request.data,'email','password')
+        key_safes, error_message = are_keys_in_dict(request.data, 'email', 'password')
         if key_safes:
             email = request.data['email']
             # Checks if there is an user with client email
-            users=UserOwnModel.objects.filter(email=email)
-            if len(users)!=0:
-                password=request.data['password']
-                user=users[0]
+            users = UserOwnModel.objects.filter(email=email)
+            if len(users) != 0:
+                password = request.data['password']
+                user = users[0]
                 # Checks if the client´s provided password is correct
                 if user.check_password(password):
-                    request.data['username']=user.username
+                    request.data['username'] = user.username
                     return super().post(request)
                 # Incorrect password
-                return Response({'message':'incorrect password'},status=400)
+                return Response({'message': 'incorrect password', 'field': 'password'}, status=400)
             # Email unused
-            return Response({'message':f'email {email} unused'},status=400)
+            return Response({'message': f'email {email} unused', 'field': 'email'}, status=400)
         # Email or password fields unprovided
-        return Response({'message':error_message},status=400)
+        return Response({'message': error_message, 'field': 'email/password'}, status=400)
 
 class UsersAPIView(APIView):
     '''
         User crud management
     '''
     # Personalization of permissions for an unlogged client can register
-    permission_classes=[AllowAny]
-    def post(self,request:HttpRequest):
+    permission_classes = [AllowAny]
+    def post(self, request: HttpRequest):
         '''
             Manage the registering of an user
         '''
         # Checks if all the keys are in dict
-        keys_safe,error_message=are_keys_in_dict(request.data,'username','password','email')
+        keys_safe, error_message = are_keys_in_dict(request.data, 'username', 'password', 'email')
         if keys_safe:
-            username=request.data['username']
-            email=request.data['email']
-            password=request.data['password']
+            username = request.data['username']
+            email = request.data['email']
+            password = request.data['password']
             # Checks if username or email have an user
-            username_or_email_used,error_message=UserOwnModel.email_or_username_used(username,email)
+            username_or_email_used, error_message = UserOwnModel.email_or_username_used(username, email)
             if not username_or_email_used:
                 # Checks if the password is correct
-                password_safe,error_message=UserOwnModel.safe_password(password)
+                password_safe, error_message = UserOwnModel.safe_password(password)
                 if password_safe:
                     # Saves the user and returns its information
-                    user=UserOwnModel(email=request.data['email'],username=request.data['username'],password=make_password(password))
+                    user = UserOwnModel(email=request.data['email'], username=request.data['username'], password=make_password(password))
                     user.save()
-                    serializer=UserSerializer(user,many=False)
-                    return Response(serializer.data,status=200)
+                    serializer = UserSerializer(user, many=False)
+                    return Response(serializer.data, status=200)
                 # Password incorrect, returns the right error_message (seen in UserOwnModel.safe_password function)
-                return Response({'message':error_message},status=400)
+                return Response({'message': error_message, 'field': 'password'}, status=400)
             # Username or email used, with right error_message (seen in UserOwnModel.email_or_username_used function)
-            return Response({'message':error_message},status=400)
+            return Response({'message': error_message, 'field': 'username/email'}, status=400)
         # One or more keys (username, password, email) are not provided by the client, with the right error_message (seen in are_keys_in_dict function)
-        return Response({'message':error_message},status=400)
+        return Response({'message': error_message, 'field': 'username/password/email'}, status=400)
 
 class ForgotPasswordAPIView(APIView):
     '''
@@ -110,11 +110,11 @@ class ForgotPasswordAPIView(APIView):
                 )
                 email.attach_alternative(html_content, "text/html")
                 email.send()
-                return Response({'message':'Email sent'}, status=200)
+                return Response({'message': 'Email sent'}, status=200)
             # Email not found
-            return Response({'message':"Email not found"}, status=400)
+            return Response({'message': "Email not found", 'field': 'email'}, status=400)
         # Email field not provided
-        return Response({'message':error_message}, status=400)
+        return Response({'message': error_message, 'field': 'email'}, status=400)
 
     def put(self, request: HttpRequest):
         '''
@@ -139,15 +139,15 @@ class ForgotPasswordAPIView(APIView):
                         user.password = make_password(new_password)
                         user.save()
                         pass_res_req.delete()
-                        return Response({'message':"Password changed successfully"}, status=200)
+                        return Response({'message': "Password changed successfully"}, status=200)
                     # New password not secure
-                    return Response({'message':error_message}, status=400)
+                    return Response({'message': error_message, 'field': 'new_password'}, status=400)
                 # New password and confirmation do not match
-                return Response({'message':"New password and confirmation do not match"}, status=400)
+                return Response({'message': "New password and confirmation do not match", 'field': 'new_password_confirmation'}, status=400)
             # Invalid code
-            return Response({'message':f"Code {code} is not valid"}, status=400)
+            return Response({'message': f"Code {code} is not valid", 'field': 'user_code'}, status=400)
         # Required fields not provided
-        return Response({'message':error_message}, status=400)
+        return Response({'message': error_message, 'field': 'user_code/new_password/new_password_confirmation'}, status=400)
 
 class ResetPasswordAPIView(APIView):
     '''
@@ -177,12 +177,12 @@ class ResetPasswordAPIView(APIView):
                     if password_safe:
                         user.password = make_password(new_password)
                         user.save()
-                        return Response({'message':"Password changed successfully"}, status=200)
+                        return Response({'message': "Password changed successfully"}, status=200)
                     # New password not secure
-                    return Response({'message':error_message}, status=400)
+                    return Response({'message': error_message, 'field': 'new_password'}, status=400)
                 # New password and confirmation do not match
-                return Response({'message':"New password and confirmation do not match"}, status=400)
+                return Response({'message': "New password and confirmation do not match", 'field': 'new_password_confirmation'}, status=400)
             # Old password incorrect
-            return Response({'message':'Incorrect old password'}, status=400)
+            return Response({'message': 'Incorrect old password', 'field': 'old_password'}, status=400)
         # Required fields not provided
-        return Response({'message':error_message}, status=400)
+        return Response({'message': error_message, 'field': 'old_password/new_password/new_password_confirmation'}, status=400)
